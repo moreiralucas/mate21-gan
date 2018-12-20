8b2400adbe5331ba243cd557082ea7fb2bfceea4
Lucas Moreira <moreiralucascc@gmail.com>
2018-12-20 18:00:29 -0300

    Adiciona path do tensoboard na classe parâmetros

--- source/parameters.py
+++ source/parameters.py
@@ -14,12 +14,13 @@ class Parameters:
         self.IMAGE_WIDTH = 64   # width of the image / cols
         self.NUM_CHANNELS = 1   # number of channels of the image
         # Database
-        self.TRAIN_FOLDER = '../data_part1/train' # folder with training images
-        self.TEST_FOLDER = '../data_part1/test'   # folder with testing images
+        self.TRAIN_FOLDER = 'data_part1/train' # folder with training images
+        self.TEST_FOLDER = 'data_part1/test'   # folder with testing images
         self.SPLIT_RATE = 0.80        # split rate for training and validation sets
         # Training loop
-        self.LOG_DIR = 'model/'
-        self.NUM_EPOCHS_FULL = 5000
+        self.LOG_DIR_MODEL = 'model/'
+        self.TENSORBOARD_DIR = 'tensorboard/'
+        self.NUM_EPOCHS_FULL = 50000
         self.S_LEARNING_RATE_FULL = 0.0003
         self.F_LEARNING_RATE_FULL = 0.0003
         self.BATCH_SIZE = 128
